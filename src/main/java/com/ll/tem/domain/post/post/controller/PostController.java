c8b73e891f815e95588d59123feedab32efdfe08
seeyeon <gulf72pr@swu.ac.kr>
2024-12-17 10:48:33 +0900

    write

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -14,11 +14,24 @@ public class PostController {
 	public String showWrite(){
 		return """
 				<form>
-				<form action="/posts/write">
+				<form action="/posts/doWrite">
 					<input type="text" name="title" placeholder="제목">
 					<textarea name="content" placeholder="내용"></textarea>
 					<button type="submit">글쓰기</button>
 				</form>
 				""";
+	}
+
+	@GetMapping("/doWrite")
+	@ResponseBody
+	public String write(String title,String content){
+		return """
+				 <h1>글쓰기 완료</h1>
+				
+				 <div>
+				   <h2>%s</h2>
+				   <p>%s</p>
+				   </div>
+				""".formatted(title,content);
 	}
 }
